acf7c6e8cd7e4ad331dd59937e39e090e1e7f631
Kartik Gautam <95581576+kartikgautamz@users.noreply.github.com>
2024-09-17 19:58:20 +0530

    Add files via upload

---
+++ H4X0R News/Models/PostData.swift
@@ -0,0 +1,22 @@
+//
+//  PostData.swift
+//  H4X0R News
+//
+//  Created by Kartik Gautam on 05/07/2024.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
